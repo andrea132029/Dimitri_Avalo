f8473f1380fd97d520ae1b7bb46a986ff42f1c93
andrea132029 <dimitriandy20@gmail.com>
2025-10-27 10:55:16 -0300

    Primera versión del proyecto base TP03

---
+++ src/main/java/tp03Daos/DimitriAvaloApplication.java
@@ -0,0 +1,13 @@
+package tp03Daos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimitriAvaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimitriAvaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tp03Daos/DimitriAvaloApplicationTests.java
@@ -0,0 +1,13 @@
+package tp03Daos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimitriAvaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
